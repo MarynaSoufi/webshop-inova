42a7009a2ff7ebb06eddae79385f50f14b954813
soufieddin <eddin2209@gmail.com>
2021-06-03 20:46:36 +0200

    added login-logout-register to frontend

--- src/client/assets/js/api.js
+++
@@ -1,5 +0,0 @@
-// export const fetchCats =async () => {
-//   const data =await fetch('localhost:6002/categories');
-//   const json =await data.json();
-//   return json;
-// };
--- src/client/assets/js/main.js
+++ src/client/assets/js/main.js
@@ -5,6 +5,11 @@ import { base_url } from './consts.js';
 // <-- IMPORTS END
 
 const app = {
+
+  /**
+   * Initialize the application
+   */
+
   async init() {
     this.cacheElements();
     this.registerListeners();
@@ -12,40 +17,75 @@ const app = {
     this.fetchItems();
     this.fetchItemDetail(); 
     this.picsBorder();
- 
+    this.getProfile();
+
   },
+
+  /**
+   * Get the nedded elements from dom
+   */
+
   cacheElements(){
+    //Headr elements
+
     this.$mainNavList = document.querySelector('.mainNav');
     this.$subNavList = document.querySelector('.subNav__list');
-    this.$subNavListItems = document.querySelectorAll('.subNav__item__link')
-    this.$itemsSection = document.querySelector('.items');
-    this.$listProductsById = document.querySelector('.items_list');
-    this.$detail = document.querySelector('.detail_info');
-    this.$hero = document.querySelector('.hero');
-    this.$promo = document.querySelector('.promo');
-    this.$price = document.querySelector('.price');
-    this.$insta = document.querySelector('.insta');
-    this.$instaPics = document.querySelectorAll('.pic');
-    this.$splashContainer = document.querySelector('#splash');
-    this.$openAc = document.querySelector('.ac');
+    this.$subNavListItems = document.querySelectorAll('.subNav__item__link');
+
+    this.$openAc = document.querySelectorAll('.ac');
     this.$closeAc = document.querySelector('.account__modal__top__close');
     this.$ac = document.querySelector('.account__modal');
     this.$log = document.querySelectorAll('.log');
     this.$unlog = document.querySelector('.unlog');
+    this.$logOut = document.querySelectorAll('.logOut');
 
-      this.$loginBtn = document.querySelector('.js-login');
-      this.$createBtn = document.querySelector('.js-create');
-    
-    
-      this.$logInInputEmail = document.querySelector('.email-log');
-      this.$logInInputpass = document.querySelector('.password-log');
+    //Homepage elements
+
+    this.$itemsSection = document.querySelector('.items');
+    this.$hero = document.querySelector('.hero');
+    this.$promo = document.querySelector('.promo');
+    this.$insta = document.querySelector('.insta');
+    this.$instaPics = document.querySelectorAll('.pic');
+
+    //Homepage elements with category
+
+    this.$listProductsById = document.querySelector('.items_list');
+
+    //detail page elements
+
+    this.$detail = document.querySelector('.detail_info');
+
+
+
+    //SignUp_in page elements
+
+    this.$loginBtn = document.querySelector('.js-login');
+    this.$logInInputEmail = document.querySelector('.email-log');
+    this.$logInInputpass = document.querySelector('.password-log');
+
+    this.$createBtn = document.querySelector('.js-create');
+    this.$signUpEmail = document.querySelector('.email-signUp');
+    this.$signUppass = document.querySelector('.pass-signUp');
+
+
     
   },
+
+  /**
+   * All eventListeners here
+   */
+
   registerListeners(){
-    let log = false;
-    this.$openAc.addEventListener('click',()=>{
+
+    // close and open modal account
+
+    this.$openAc.forEach((e)=>{
+      e.addEventListener('click',()=>{
         this.$ac.style.display = 'flex';
-        if(log){
+
+        //check if if user logged in 
+
+        if(this.isAuthenticated()){
           
           this.$unlog.style.display = 'none';
           this.$log.forEach((e)=>{
@@ -65,33 +105,67 @@ const app = {
           
         }
     })
+    })
     this.$closeAc.addEventListener('click',()=>{
       this.$ac.style.display = 'none';
     })
 
-    // this.$createBtn.addEventListener('click', () => {
+    // login button on signUp_in page
 
-    // });
-    if(this.$loginBtn){
-      this.$loginBtn.addEventListener('click', async() => {
-        const email = this.$logInInputEmail.value;
-        const password = this.$logInInputpass.value;
-        this.requestInfo = { email: email, password: password };
-        await this.logIn(this.requestInfo);
-  
-      });
+    if(!this.$loginBtn){
+      return; }
+    this.$loginBtn.addEventListener('click', async(e) => {
+      e.preventDefault();
+      const email = this.$logInInputEmail.value;
+      const password = this.$logInInputpass.value;
+      this.requestInfo = { email: email, password: password };
+      await this.logIn(this.requestInfo);
+      console.log('in')
+      
+    });
+    
+    //logOut button 
+
+    this.$logOut.forEach((e)=>{
+      if(this.isAuthenticated()) {
+        e.addEventListener('click', async(e) => {
+          e.preventDefault();
+          console.log('p')
+          await this.logOut();
+          this.$ac.style.display = 'none';
+          
+        });
+      }
+    })
+    
+
+
+    // Register/Create account button on signUp_in page
+
+    if(!this.$createBtn) {
+      return;
     }
-
-    // if(this.$loginBtn){
-    //   this.$loginBtn.addEventListener('click',(e)=>{
-    //     e.preventDefault();
-    //     console.log(this.$logInInputEmail.value);
-    //     console.log(this.$logInInputpass.value);
-    //   })
-    // }
-   
+    this.$createBtn.addEventListener('click', async(e) => {
+      e.preventDefault();
+      const email = this.$signUpEmail.value;
+      const password = this.$signUppass.value;
+      this.info = { email: email, password: password };
+      await this.signUp(this.info);
+      console.log('created')
+      window.location.href = "http://127.0.0.1:5500/src/client/index.html";
+    });
   },
 
+  /**
+   * Check if there is loggened user
+   */
+  isAuthenticated() {
+    return !!window.localStorage.getItem('token');
+  },
+
+  /**
+   * get all the ictures in insta section on homepage and add border by index
+   */
   picsBorder() {
     this.$instaPics.forEach((p, index)=>{
       if(index % 2 === 0){
@@ -99,6 +173,28 @@ const app = {
       }
     });
   },
+
+  /**
+   * get all categories from db 
+   */
+
+  async fetchCats () {
+    const data =await fetch(`${base_url}/categories`);
+    const json =await data.json();
+    this.result = json.categories;
+
+    //with this data create subnav menu for categories
+
+    this.createSubNav(this.result)
+  },
+
+
+
+  /**
+   * After getting data (all categories) this function creates elements in header
+   * @param {array} result
+   */
+
   createSubNav(result){
     let str= '';
     if (this.$subNavList){
@@ -110,141 +206,188 @@ const app = {
       this.$subNavList.innerHTML = str;
     }
   },
- 
-  async fetchCats () {
-  const data =await fetch(`${base_url}/categories`);
-  const json =await data.json();
-  this.result = json.categories;
-  this.createSubNav(this.result)
-},
 
-async fetchItems() {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const category = urlParams.get('categoryId');
-  const name = urlParams.get('categoryName');
-  if(category) {
-    document.title = `INOVA | ${name}`;
-    this.$hero.style.display = 'none'
-    this.$promo.style.display = 'none'
-    this.$insta.style.display = 'none'
-    this.$itemsSection.classList.add('listGroup');
-    this.$listProductsById.classList.add('listGroup__items');
-    const data =await fetch(`${base_url}/categories/${category}/all_products`);
-    const json =await data.json();
-    this.result = json.products;
-    this.createListProductsByCategory(this.result)
-  }
-  else{
-    if(this.$itemsSection && this.$listProductsById){
-      this.$itemsSection.classList.add('listHome');
-      this.$listProductsById.classList.add('listHome__items');
-      const data =await fetch(`${base_url}/products/promo`);
+  /**
+   *get promo products if it's homepage without specific category
+   *get all products of category if on homepage chosen this category
+   */
+  async fetchItems() {
+    //get url params to check if user choosed a category
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const category = urlParams.get('categoryId');
+    const name = urlParams.get('categoryName');
+
+    //if user choosed a category so we change the layout of the home page
+
+    if(category) {
+      document.title = `INOVA | ${name}`;
+      this.$hero.style.display = 'none'
+      this.$promo.style.display = 'none'
+      this.$insta.style.display = 'none'
+      this.$itemsSection.classList.add('listGroup');
+      this.$listProductsById.classList.add('listGroup__items');
+      const data =await fetch(`${base_url}/categories/${category}/all_products`);
       const json =await data.json();
       this.result = json.products;
-      this.createListProductsByCategory(this.result.slice(0,8))
+      this.createListProductsByCategory(this.result)
     }
-    
-    
-    
-  }
+    //if user didnt choose any category so we create the layout of the home page
+    else{
+      if(this.$itemsSection && this.$listProductsById){
+        this.$itemsSection.classList.add('listHome');
+        this.$listProductsById.classList.add('listHome__items');
+        const data =await fetch(`${base_url}/products/promo`);
+        const json =await data.json();
+        this.result = json.products;
+        this.createListProductsByCategory(this.result.slice(0,8))
+      }
+    }
+  },
+
+  /**
+   * After getting data (needed products) this function creates a list of nedded products
+   * @param {array} result
+   */
   
-},
+  createListProductsByCategory(result) {
+    let str= '';
+      if (this.$listProductsById){
+        result.forEach((e)=>{
+          str+=`
+          <a href = '/src/client/detail.html?productId=${e.product_id}&prductName=${e.name}'>
+            <img src="./assets/images/${e.image}.jpg" alt="image ${e.name}" class = 'image image--list'/>
+            <strong>${e.name}</strong>
+            <div class='price'>
+              <p class='price__amount'>€${e.price}</p>
+              <p class='price__disc'>${e.promo_id > 0 ? '(you save €' + `${Math.round(e.price * 0.1)}.00)` : ''}</p>
+            </div>
+          </a>
+          `;
+        });
+        this.$listProductsById.innerHTML = str;
+      }
+  },
 
-createListProductsByCategory(result) {
-  let str= '';
-    if (this.$listProductsById){
-      result.forEach((e)=>{
-        str+=`
-        <a href = '/src/client/detail.html?productId=${e.product_id}&prductName=${e.name}'>
-          <img src="./assets/images/${e.image}.jpg" alt="image ${e.name}" class = 'image image--list'/>
-          <strong>${e.name}</strong>
-          <div class='price'>
-            <p class='price__amount'>€${e.price}</p>
-            <p class='price__disc'>${e.promo_id > 0 ? '(you save €' + `${Math.round(e.price * 0.1)}.00)` : ''}</p>
-          </div>
-         
-        </a>
-        `;
-        
-      });
-
-      this.$listProductsById.innerHTML = str;
-
+  /**
+   * if user clicked on a specific product, here we fetch this product's data details
+   */
+  async fetchItemDetail() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const product = urlParams.get('productId')
+    const name = urlParams.get('prductName');
+    if(product){
+      document.title = `INOVA | Details | ${name}`
+      const data =await fetch(`${base_url}/products/${product}`);
+      const json =await data.json();
+      this.result = json.products;
+      this.createProductDetail(this.result)
     }
-},
-async fetchItemDetail() {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('productId')
-  const name = urlParams.get('prductName');
-  if(product){
-    document.title = `INOVA | Details | ${name}`
-    const data =await fetch(`${base_url}/products/${product}`);
-    const json =await data.json();
-    this.result = json.products;
-    this.createProductDetail(this.result)
-  }
-},
-// async signUp(info){
-//   const data =await fetch(`${base_url}/auth/register`, { body: info, method: 'POST'});
-//   const json =await data.json();
-//   return json;
-// },
-async logIn(requestInfo){
-  const data =await fetch(`${base_url}/auth/login`, { body: JSON.stringify(requestInfo), method: 'POST'});
-  const json =await data.json();
-  window.localStorage.setItem('token', json.token);
-},
+  },
 
-// async logOut(){
-//   window.localStorage.removeItem('token');
-// },
+  /**
+   * After getting data (product's details) this function creates elements of this product
+   * @param {array} result
+   */
+
+  createProductDetail(result) {
+    let str= ``;
+      if (this.$detail){
+        const tag = result.tags;
+        const tagItem = tag.map(t => `<li>${t.name}</li>`).join('')
+  
+        const reviews = result.reviews;
+        const reviewItem = reviews.map(r => 
+        `<li>
+          <strong>${r.user_id}</strong>
+          <p>${r.description}</p>
+        </li>`).join('')
+        
+          str=`
+            <img src="./assets/images/${result.image}.jpg" alt="image ${result.name}" class = 'image image--detail'/>
+            <div>${result.name}</div>
+            <p>€${result.price}</p>
+            <p>${result.description}</p>
+            <ul>${tagItem}</ul>
+            <p>Reviews: ${reviews.length}</p>
+            <ul>${reviewItem}</ul>
+  
+          `;
+        this.$detail.innerHTML = str;
+      }
+  },
+
+  /**
+   * here new user register him self after filling the registering form 
+   * and pressing on create account button 
+   * we get the data as param
+   * @param {string} info
+   * and post them to db table users
+   */
+
+  async signUp(info){
+    const data =await fetch(`${base_url}/users`, { body: JSON.stringify(info), method: 'POST',
+    headers: {'Content-Type': 'application/json'}});
+    const json =await data.json();
+    return json;
+  },
+
+    /**
+   * here new user log in  after filling the signIn form 
+   * and pressing on login button 
+   * we get the data as param
+   * @param {string} requestInfo
+   * and post them to db for checking
+   * we get token as reponse and we save this token in local storage to check if user logged in 
+   */
+
+  async logIn(requestInfo){
+    const data =await fetch(`${base_url}/auth/login`, { body: JSON.stringify(requestInfo), method: 'POST',
+    headers: {'Content-Type': 'application/json'}});
+    const json =await data.json();
+    window.localStorage.setItem('token', json.token);
+    window.location.href = "http://127.0.0.1:5500/src/client/index.html";
+  },
+
+  /**
+   * here logged in user can logou
+   * by pressing on account button and choosing logout button 
+   * we delete the token from local storage
+   */
+  async logOut(){
+    window.localStorage.removeItem('token');
+    window.location.href = "http://127.0.0.1:5500/src/client/signUp_In.html";
+  },
+
 // async getCart() {
 //   return await call(`${base_url}/cart`, 'GET');
 // },
+async getProfile() {
+  const result =  await this.call(`${base_url}/users/profile`, 'GET');
+  const profile = result.profile
+  console.log(profile)
+  return profile;
+},
 // async addToCart(productId) {
 //   const body = { productId: productId};
 //   return await call(`${base_url}/cart/products`, 'POST', body);
 // },
-// async call(url, method, body){
-//   const token = window.localStorage.getItem('token');
-//   const options = { method: method, headers: { 'Authorization': `Bearer ${token}`}};
-//   if(body) {
-//     options.body = body;
-//   }
-//   const data =await fetch(url, options);
-//   const json =await data.json();
-//   return json;
-// },
-createProductDetail(result) {
-  let str= ``;
-    if (this.$detail){
-      const tag = result.tags;
-      const tagItem = tag.map(t => `<li>${t.name}</li>`).join('')
+async call(url, method, body){
+  const token = window.localStorage.getItem('token');
+  const options = { method: method, headers: { 'Authorization': `Bearer ${token}`}};
+  if(body) {
+    options.body = body;
+  }
+  const data =await fetch(url, options);
+  const json =await data.json();
+  if(json.name === 'TokenExpiredError'){
+    await this.logOut();
+  }
 
-      const reviews = result.reviews;
-      const reviewItem = reviews.map(r => 
-      `<li>
-        <strong>${r.user_id}</strong>
-        <p>${r.description}</p>
-      </li>`).join('')
-      
-        str=`
-          <img src="./assets/images/${result.image}.jpg" alt="image ${result.name}" class = 'image image--detail'/>
-          <div>${result.name}</div>
-          <p>€${result.price}</p>
-          <p>${result.description}</p>
-          <ul>${tagItem}</ul>
-          <p>Reviews: ${reviews.length}</p>
-          <ul>${reviewItem}</ul>
+  return json;
+},
 
-        `;
-
-      this.$detail.innerHTML = str;
-
-    }
-}
 }
 // start the app
 app.init();
